20b3ebefa6398efe615700beb24a3125226157bb
Minakshi Barabde <minakshibarabde@gmail.com>
2026-03-09 00:56:33 +0530

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+Public class ReverseNumber {
+  Public Static Void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number");
+    int num = sc.nextInt();
+
+    int reverse = 0;
+    while(num !=0){
+      int digit = num % 10;
+      reverse =reverse * 10 + digit;
+      num = num/10;
+    }
+    System.out.println("Reversed number:" + reverse);
+  }
+}
